8ccf4f46b948aa5602fe7f38e44e18ecfb7d1af0
kien189 <kien18092004@gmail.com>
2023-11-17 10:23:21 +0700

    kien189

--- signin_gia_huy.php
+++ signin_gia_huy.php
@@ -45,7 +45,7 @@
                                         <label for="formCheck" class="form-check-label text-secondary"><small>Remember Me</small></label>
                                     </div>
                                     <div class="forgot">
-                                        <small><a href="#" style="text-decoration: none;">Forgot Password?</a></small>
+                                        <small><a href="./quenmk.php" style="text-decoration: none;">Forgot Password?</a></small>
                                     </div>
                                 </div>
                                 <div class="input-group mb-3">
